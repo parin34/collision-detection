5861c71872201fb620711ee864d4d13b3501bafb
parin34 <72636755+parin34@users.noreply.github.com>
2020-10-20 20:56:03 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedrect,movingrect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect = createSprite(400, 200, 60, 40);
+
+  movingrect = createSprite(500,300,40,60);
+}
+
+
+function draw() {
+  background(255,255,255);  
+  
+  movingrect.x=mouseX
+  movingrect.y=mouseY
+ 
+  if(movingrect.x - fixedrect.x < movingrect.width/2 + fixedrect.width/2 &&
+    fixedrect.x - movingrect.x < movingrect.width/2 + fixedrect.width/2 &&
+    movingrect.y - fixedrect.y < movingrect.height/2 + fixedrect.height/2 &&
+    fixedrect.y - movingrect.y < movingrect.height/2 + fixedrect.height/2)
+    {
+    movingrect.shapeColor="red"
+    fixedrect.shapeColor="red"
+
+  }
+  else {
+    movingrect.shapeColor="green"
+    fixedrect.shapeColor="green"
+  }
+
+  drawSprites();
+}
